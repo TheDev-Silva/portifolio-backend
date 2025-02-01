c5eda3e62c93e9a179fbee46b60341d7cd3fd284
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-02-01 20:06:43 -0300

    feat: ajustando rota inicial

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ import { Routes } from './routes';
 
 
 const app = Fastify();
-
+app.get('/', (req, reply) => {
+  reply.send({ message: 'API funcionando!' });
+});
 app.register(Routes);
 app.register(cors)
 
