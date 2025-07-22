911c27f9a26b911cb41f472899b6551dbd44a624
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-07-22 05:57:06 -0300

    build

--- backend-capture/lib/prisma.ts
+++ backend-capture/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+/* import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -10,4 +10,4 @@ export const prisma =
     log: ['query', 'info', 'warn', 'error'],
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma */
