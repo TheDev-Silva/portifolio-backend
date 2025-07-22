4055d4927cfe36eb036822ba1d68ebb95c85e50c
TheDev-Silva <155161430+TheDev-Silva@users.noreply.github.com>
2025-07-22 12:48:40 -0300

    Atualizar o server.ts

--- backend-capture/src/server.ts
+++ backend-capture/src/server.ts
@@ -7,45 +7,37 @@ dotenv.config();
 
 const fastify = Fastify();
 
-
 // Registra o CORS
 fastify.register(cors);
 
-// Registra as rotas
+// Rota raiz
 fastify.get('/', async (req, reply) => {
-    return { message: 'API funcionando!' };
+  return { message: 'API funcionando!' };
 });
 
+// Registra as rotas
 fastify.register(clientRoutes);
 
-
-// Exporta como uma função para Vercel
-/* export default async (req: any, res: any) => {
-    await fastify.ready();
-    fastify.server.emit('request', req, res);
-}; */
-
+// Exporta handler para Vercel
 export default async (req: any, res: any) => {
   try {
-    // Certifica-se de que o Fastify está pronto para processar as requisições
     await fastify.ready();
-    // Encaminha a requisição para o servidor Fastify
     fastify.server.emit('request', req, res);
   } catch (err) {
-    // Em caso de erro, envia uma resposta diretamente
     res.statusCode = 500;
     res.end('Internal Server Error');
     console.error(err);
   }
 };
 
-
-// Inicia o servidor
-fastify.listen({port: 3333, host: '0.0.0.0'}, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
-
+// Somente inicializa o servidor se não estiver rodando no Vercel
+if (process.env.VERCEL !== '1') {
+  const port = process.env.PORT || 3333;
+  fastify.listen({ port, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}
